d8e1ea2b75250a023c019dec859c110137c31340
Luan Guimarães <livreluan@gmail.com>
2016-11-22 14:52:56 -0200

    Refactoring routes

--- config/routes.js
+++ config/routes.js
@@ -5,56 +5,37 @@
 
 'use strict';
 
-var User = require('../src/controllers/users_controller.js')
-var Event = require('../src/controllers/events_controller.js')
+var User = require('../src/controllers/users_controller.js');
+var Event = require('../src/controllers/events_controller.js');
 
-module.exports=function(app, base) {
-  
-  //user
- 
+module.exports = function(app, base) {
+  resourcesFor(app, base, 'users', User);
+  resourcesFor(app, base, 'events', Event);
+}
+
+function resourcesFor(app, base, name, resource){
   //create
-  app.post(base+'/user', function (req, res) {
-    User.create(req,res)
-  })
-  //show
-  app.get(base+'/user/:id', function (req, res) {
-    User.show(req,res)
-  })
-  //update
-  app.put(base+'/user/:id', function (req, res) {
-    User.update(req,res)
-  })
-  //destroy
-  app.delete(base+'/user/:id', function (req, res) {
-    User.destroy(req,res)
-  })
-  //show all
-  app.get(base+'/users', function (req, res) {
-    User.getAll(req,res)
-  })
+  app.post(base + '/' + name , function (req, res) {
+    resource.create(req,res);
+  });
 
-  //event
- 
-  //create
-  app.post(base+'/event', function (req, res) {
-    Event.create(req,res)
-  })
   //show
-  app.get(base+'/event/:id', function (req, res) {
-    Event.show(req,res)
-  })
-  //update
-  app.put(base+'/event/:id', function (req, res) {
-    Event.update(req,res)
-  })
-  //destroy
-  app.delete(base+'/event/:id', function (req, res) {
-    Event.destroy(req,res)
-  })
-  //show all
-  app.get(base+'/events', function (req, res) {
-    Event.getAll(req,res)
-  })
+  app.get(base + '/' + name + '/:id', function (req, res) {
+    resource.show(req,res);
+  });
 
-  
+  //update
+  app.put(base + '/' + name + '/:id', function (req, res) {
+    resource.update(req,res);
+  });
+
+  //destroy
+  app.delete(base + '/' + name + '/:id', function (req, res) {
+    resource.destroy(req,res);
+  });
+
+  //show all
+  app.get(base + '/' + name, function (req, res) {
+    resource.getAll(req,res);
+  });
 }
