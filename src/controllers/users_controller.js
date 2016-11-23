2e3b5cef7f613495b642d35cc9c78922bbe90094
Tallys Martins <tallysmartins@gmail.com>
2016-11-23 16:57:17 +0100

    Adds users CRUD
     - The basic CRUD is already working.
    
    Signed-off-by: Maurilio Atila <cabelotaina@gmail.com>
    Signed-off-by: Tallys Martins <tallysmartins@gmail.com>

--- src/controllers/users_controller.js
+++ src/controllers/users_controller.js
@@ -1,4 +1,4 @@
-/*!
+/*i!
  * pushing together
  * GNU 3.0 Licensed
  */
@@ -11,7 +11,6 @@ module.exports = function() {
 
   var create = function(req,res){
     setImmediate(function () {
-      //TODO: call model add
       var jsonStr = '{"action":"create user", "id": "1", "name": "Maurilio Atila"}';
       try {
         var jsonObj = JSON.parse(jsonStr);
@@ -24,53 +23,80 @@ module.exports = function() {
   
   var show = function(req,res){
     setImmediate(function () {
-      // TODO: call model show
-      var jsonStr = '{"action":"show user", "id": '+req.params.id+', "name": "Maurilio Atila"}';
-      try {
-        var jsonObj = JSON.parse(jsonStr);
-        res.send(jsonObj);
-      } catch (e) {
-        res.status(400).send('Invalid JSON string');
-      }
+      models.User.findById(req.params.id)
+      .then(function (user) {
+          if(user) {
+              res.status(200).send(user);
+          }else {
+            res.status(404).send('User not found');
+          }
+        })
+
+      .error(function(err){
+        res.status(500).send('Internal server error');
+      });
     });
   }
 
   var update = function(req,res){ 
     setImmediate(function () {
-      //TODO: call model update
-      var jsonStr = '{"action":"update user", "id": '+req.params.id+', "name": "Maurilio Atila"}';
-      try {
-        var jsonObj = JSON.parse(jsonStr);
-        res.send(jsonObj);
-      } catch (e) {
-        res.status(400).send('Invalid JSON string');
-      }
+      models.User.findById(req.params.id)
+      .then(function (user) {
+          if(user) {
+            user.updateAttributes(req.body)
+            .then(function (u) {
+              res.status(200).send(user);
+            })
+
+            .error(function () {
+              res.status(400).send('Error updating the user');
+            });
+          }else {
+            res.status(404).send('User not found');
+          }
+        })
+
+      .error(function(err){
+        res.status(500).send('Internal server error');
+      });
     });
   }
   
   var destroy = function(req,res){
     setImmediate(function () {
-      //TODO: call model delete and see if user exists
-      try {
-        res.send('Success');
-      } catch (e) {
-        res.status(400).send('Invalid JSON string');
-      }
+      models.User.findById(req.params.id)
+      .then(function (user) {
+        if(user) {
+          user.destroy()
+          .then(function (u) {
+            res.status(200).send(user);
+          })
+
+          .error(function () {
+            res.status(400).send('Error destroying the user');
+          });
+        }else {
+          res.status(404).send('User not found');
+        }
+      })
+
+      .error(function(err){
+        res.status(500).send('Internal server error');
+      });
     });
   }
 
   var getAll = function(req,res){
     setImmediate(function () {
-      //TODO: call model getAll
-      var jsonStr = '{"users": [{"action":"update user", "id": 2, "name": "Maurilio Atila"}, {"action":"update user", "id": 2, "name": "Henrique Parra"}]}';
-      try {
-        var jsonObj = JSON.parse(jsonStr);
-        res.send(jsonObj);
-      } catch (e) {
-        res.status(400).send('Invalid JSON string');
-      }
-    });
+      models.User.all()
+      .then(function (users) {
+        res.status(200).send(users);
+      })
 
+      .error(function(err){
+        res.status(500).send('Internal server error');
+      });
+    });
   }
 
   return {
