f1d66d813d0db83f8637b8f64fbbd8f84da8d6c2
Luan Guimarães <livreluan@gmail.com>
2016-11-29 10:26:57 -0200

    Creating migrations for Conversation

---
+++ db/migrations/20161129120504-create-conversation.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    queryInterface.createTable(
+      'Conversations',
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+        },
+
+        createdAt: {
+          type: Sequelize.DATE
+        },
+
+        updatedAt: {
+          type: Sequelize.DATE
+        },
+      
+        externalUrl: Sequelize.STRING,
+
+        ownerId: {
+          type: Sequelize.INTEGER,
+          references: {
+            model: "Users",
+            key: "id"
+          }
+        },
+      },
+      {
+        engine: 'MYISAM', // default: 'InnoDB'
+        charset: 'utf8' // default: null
+      }
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    queryInterface.dropTable('Conversations');
+  }
+};
